059cd0c296201f3b7fe9b12e6024195875c600cf
Chris Ekstedt <9835700+cekstedt@users.noreply.github.com>
2022-06-06 15:08:08 -0700

    Added POST function.

--- app.js
+++ app.js
@@ -3,13 +3,21 @@ const request = require("request");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.use(express.static("public"));
 const port = 3000;
 
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res) {
+  let firstName = req.body.firstName;
+  let lastName = req.body.lastName;
+  let email = req.body.email;
+
+  console.log(firstName + " " + lastName + ": " + email);
 });
 
 app.listen(port, function() {
