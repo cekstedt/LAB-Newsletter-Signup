c7ec9fac732c07d505e71c740fce33ad2d0a65aa
Chris Ekstedt <9835700+cekstedt@users.noreply.github.com>
2022-06-07 14:07:37 -0700

    Added failure and success pages.

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/signup.html");
@@ -26,16 +28,14 @@ app.post("/", function(req, res) {
   const email = req.body.email;
 
   const data = {
-    members: [
-      {
-        email_address: email,
-        status: "subscribed",
-        merge_fields: {
-          FNAME: firstName,
-          LNAME: lastName
-        }
+    members: [{
+      email_address: email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: firstName,
+        LNAME: lastName
       }
-    ]
+    }]
   };
   const jsonData = JSON.stringify(data);
 
@@ -46,8 +46,14 @@ app.post("/", function(req, res) {
   };
 
   const request = https.request(url, options, function(response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+
     response.on("data", function(data) {
-      console.log(JSON.parse(data));
+      // console.log(JSON.parse(data));
     })
   })
 
@@ -56,9 +62,10 @@ app.post("/", function(req, res) {
 
 });
 
+app.post("/failure", function(req, res) {
+  res.redirect("/");
+})
+
 app.listen(port, function() {
   console.log("Server started on port " + port);
 });
-
-// List ID
-//
